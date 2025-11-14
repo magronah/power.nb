b979c3a1d20265a5df7c0298c4d263c5d3dc5363
Ben Bolker <bbolker@gmail.com>
2025-11-14 15:24:34 -0500

    roxygen comments

--- R/sample_size_cal.R
+++ R/sample_size_cal.R
@@ -1,167 +1,163 @@
+#
+# #######################################################################
+# inverse_fun <- function(target,lmb,abs_lfc, model,xmin, xmax) {
+#
+#   if (length(target) != length(lmb) || length(target) != length(abs_lfc)) {
+#     stop("Lengths of 'target', 'lmb', and 'abs_lfc' must be the same.")
+#   }
+#
+#   # Initialize an empty list to store the roots
+#   roots <- list()
+#
+#   # Loop through each element and solve for the root
+#   for (i in 1:length(target)) {
+#     roots[[i]] <- uniroot(function(s) {
+#       predict(model,
+#               type = "response",
+#               newdata = data.frame(sample_size = s,
+#                                    lmean_abund = lmb[i],
+#                                    abs_lfc = abs_lfc[i])
+#       ) - target[i]
+#     },
+#     interval = c(xmin, xmax),
+#     extendInt = "yes")$root
+#   }
+#
+#   unlist(roots)
+# }
+#
+#
+# uniroot_lmb =  function(target_power,sample_size,abs_lfc,model,xmin,xmax){
+#
+#   root <- uniroot(function(lmb) {
+#     predict(model,
+#             type = "response",
+#             newdata = data.frame(sample_size = sample_size,
+#                                  lmean_abund = lmb,
+#                                  abs_lfc = abs_lfc)
+#     ) - target_power
+#   },
+#   interval = c(xmin, xmax),
+#   extendInt = "yes")$root
+#   root
+# }
+#
+#
+#
+# inverse_lfc <- function(target,lmb,samp_size, model,xmin, xmax) {
+#
+#   if (length(target) != length(lmb) || length(target) != length(samp_size)) {
+#     stop("Lengths of 'target', 'lmb', and 'abs_lfc' must be the same.")
+#   }
+#
+#   # Initialize an empty list to store the roots
+#   roots <- list()
+#
+#   # Loop through each element and solve for the root
+#   for (i in 1:length(target)) {
+#     roots[[i]] <- uniroot(function(s) {
+#       predict(model,
+#               type = "response",
+#               newdata = data.frame(sample_size =  samp_size[i],
+#                                    lmean_abund =  lmb[i],
+#                                    abs_lfc     =  s)
+#       ) - target[i]
+#     },
+#     interval = c(xmin, xmax),
+#     extendInt = "yes")$root
+#   }
+#
+#   unlist(roots)
+# }
+#
+#
+#
+# inverse_lmb <- function(target,abs_lfc,samp_size, model,xmin, xmax) {
+#
+#   if (length(target) != length(abs_lfc) || length(target) != length(samp_size)) {
+#     stop("Lengths of 'target', 'lmb', and 'abs_lfc' must be the same.")
+#   }
+#
+#   # Initialize an empty list to store the roots
+#   roots <- list()
+#
+#   # Loop through each element and solve for the root
+#   for (i in 1:length(target)) {
+#     roots[[i]] <- uniroot(function(lmb) {
+#       predict(model,
+#               type = "response",
+#               newdata = data.frame(sample_size =  samp_size[i],
+#                                    lmean_abund =  lmb,
+#                                    abs_lfc     =  abs_lfc[i])
+#       ) - target[i]
+#     },
+#     interval = c(xmin, xmax),
+#     extendInt = "yes")$root
+#   }
+#
+#   unlist(roots)
+# }
+#
+#
+#
+#
+#
+# uniroot_ss =  function(target_power,lmean_abund, abs_lfc,model,xmin,xmax){
+#
+#   root <- uniroot(function(ss) {
+#     predict(model,
+#             type = "response",
+#             newdata = data.frame(sample_size = ss,
+#                                  lmean_abund = lmean_abund,
+#                                  abs_lfc = abs_lfc)
+#     ) - target_power
+#   },
+#   interval = c(xmin, xmax),
+#   extendInt = "yes")$root
+#   root
+# }
+#
+# # Function to compute the missing input
+# power.nb <- function(power, sample_size, logfoldchange, logmean_abundance, gam_mod) {
+#
+#   if (is.numeric(power) && is.numeric(sample_size) && is.numeric(logfoldchange) && is.numeric(logmean_abundance)) {
+#     if (!missing(power) && !missing(sample_size) && !missing(logfoldchange)) {
+#       if (missing(logmean_abundance)) {
+#         # Compute the missing input
+#
+#       }
+#     } else if (!missing(power) && !missing(sample_size) && !missing(logmean_abundance)) {
+#       # Compute the missing input
+#       logfoldchange <- logmean_abundance - power - sample_size
+#     } else if (!missing(power) && !missing(logfoldchange) && !missing(logmean_abundance)) {
+#       # Compute the missing input
+#       sample_size <- logmean_abundance - power - logfoldchange
+#     } else if (!missing(sample_size) && !missing(logfoldchange) && !missing(logmean_abundance)) {
+#       # Compute the missing input
+#       power <- logmean_abundance - sample_size - logfoldchange
+#     } else {
+#       stop("Please specify at least 3 inputs.")
+#     }
+#
+#     # Return the computed inputs
+#     return(list(power = power, sample_size = sample_size, logfoldchange = logfoldchange, logmean_abundance = logmean_abundance))
+#   } else {
+#     stop("Inputs must be numeric values.")
+#   }
+# }
+#
+#
+#
+#
+#' Power function
 #'
-#' #######################################################################
-#' inverse_fun <- function(target,lmb,abs_lfc, model,xmin, xmax) {
-#'
-#'   if (length(target) != length(lmb) || length(target) != length(abs_lfc)) {
-#'     stop("Lengths of 'target', 'lmb', and 'abs_lfc' must be the same.")
-#'   }
-#'
-#'   # Initialize an empty list to store the roots
-#'   roots <- list()
-#'
-#'   # Loop through each element and solve for the root
-#'   for (i in 1:length(target)) {
-#'     roots[[i]] <- uniroot(function(s) {
-#'       predict(model,
-#'               type = "response",
-#'               newdata = data.frame(sample_size = s,
-#'                                    lmean_abund = lmb[i],
-#'                                    abs_lfc = abs_lfc[i])
-#'       ) - target[i]
-#'     },
-#'     interval = c(xmin, xmax),
-#'     extendInt = "yes")$root
-#'   }
-#'
-#'   unlist(roots)
-#' }
-#'
-#'
-#' uniroot_lmb =  function(target_power,sample_size,abs_lfc,model,xmin,xmax){
-#'
-#'   root <- uniroot(function(lmb) {
-#'     predict(model,
-#'             type = "response",
-#'             newdata = data.frame(sample_size = sample_size,
-#'                                  lmean_abund = lmb,
-#'                                  abs_lfc = abs_lfc)
-#'     ) - target_power
-#'   },
-#'   interval = c(xmin, xmax),
-#'   extendInt = "yes")$root
-#'   root
-#' }
-#'
-#'
-#'
-#' inverse_lfc <- function(target,lmb,samp_size, model,xmin, xmax) {
-#'
-#'   if (length(target) != length(lmb) || length(target) != length(samp_size)) {
-#'     stop("Lengths of 'target', 'lmb', and 'abs_lfc' must be the same.")
-#'   }
-#'
-#'   # Initialize an empty list to store the roots
-#'   roots <- list()
-#'
-#'   # Loop through each element and solve for the root
-#'   for (i in 1:length(target)) {
-#'     roots[[i]] <- uniroot(function(s) {
-#'       predict(model,
-#'               type = "response",
-#'               newdata = data.frame(sample_size =  samp_size[i],
-#'                                    lmean_abund =  lmb[i],
-#'                                    abs_lfc     =  s)
-#'       ) - target[i]
-#'     },
-#'     interval = c(xmin, xmax),
-#'     extendInt = "yes")$root
-#'   }
-#'
-#'   unlist(roots)
-#' }
-#'
-#'
-#'
-#' inverse_lmb <- function(target,abs_lfc,samp_size, model,xmin, xmax) {
-#'
-#'   if (length(target) != length(abs_lfc) || length(target) != length(samp_size)) {
-#'     stop("Lengths of 'target', 'lmb', and 'abs_lfc' must be the same.")
-#'   }
-#'
-#'   # Initialize an empty list to store the roots
-#'   roots <- list()
-#'
-#'   # Loop through each element and solve for the root
-#'   for (i in 1:length(target)) {
-#'     roots[[i]] <- uniroot(function(lmb) {
-#'       predict(model,
-#'               type = "response",
-#'               newdata = data.frame(sample_size =  samp_size[i],
-#'                                    lmean_abund =  lmb,
-#'                                    abs_lfc     =  abs_lfc[i])
-#'       ) - target[i]
-#'     },
-#'     interval = c(xmin, xmax),
-#'     extendInt = "yes")$root
-#'   }
-#'
-#'   unlist(roots)
-#' }
-#'
-#'
-#'
-#'
-#'
-#' uniroot_ss =  function(target_power,lmean_abund, abs_lfc,model,xmin,xmax){
-#'
-#'   root <- uniroot(function(ss) {
-#'     predict(model,
-#'             type = "response",
-#'             newdata = data.frame(sample_size = ss,
-#'                                  lmean_abund = lmean_abund,
-#'                                  abs_lfc = abs_lfc)
-#'     ) - target_power
-#'   },
-#'   interval = c(xmin, xmax),
-#'   extendInt = "yes")$root
-#'   root
-#' }
-#'
-#' # Function to compute the missing input
-#' power.nb <- function(power, sample_size, logfoldchange, logmean_abundance, gam_mod) {
-#'
-#'   if (is.numeric(power) && is.numeric(sample_size) && is.numeric(logfoldchange) && is.numeric(logmean_abundance)) {
-#'     if (!missing(power) && !missing(sample_size) && !missing(logfoldchange)) {
-#'       if (missing(logmean_abundance)) {
-#'         # Compute the missing input
-#'
-#'       }
-#'     } else if (!missing(power) && !missing(sample_size) && !missing(logmean_abundance)) {
-#'       # Compute the missing input
-#'       logfoldchange <- logmean_abundance - power - sample_size
-#'     } else if (!missing(power) && !missing(logfoldchange) && !missing(logmean_abundance)) {
-#'       # Compute the missing input
-#'       sample_size <- logmean_abundance - power - logfoldchange
-#'     } else if (!missing(sample_size) && !missing(logfoldchange) && !missing(logmean_abundance)) {
-#'       # Compute the missing input
-#'       power <- logmean_abundance - sample_size - logfoldchange
-#'     } else {
-#'       stop("Please specify at least 3 inputs.")
-#'     }
-#'
-#'     # Return the computed inputs
-#'     return(list(power = power, sample_size = sample_size, logfoldchange = logfoldchange, logmean_abundance = logmean_abundance))
-#'   } else {
-#'     stop("Inputs must be numeric values.")
-#'   }
-#' }
-#'
-#'
-#'
-#'
-#' #' Title
-#' #'
-#' #' @param deseq_est_list a list containing fold change, pvalues and other estimates from `DESeq2`
-#' #' @param true_logfoldchange  list containing simulated log fold change used  for simulating the count data
-#' #' @param true_logmean list containing simulated log mean counte used  for simulating the count data
-#' #' @param sample_vec vector of sample sizes
-#' #' @param alpha_level sign containing simulated log fold change used  for simulating the count data
-#' #' @param notu number of OTUs
-#' #'
-#' #' @return
-#' #' @export
-#' #'
-#' #' @examples
+#' @param deseq_est_list a list containing fold change, pvalues and other estimates from `DESeq2`
+#' @param true_logfoldchange  list containing simulated log fold change used  for simulating the count data
+#' @param true_logmean list containing simulated log mean counte used  for simulating the count data
+#' @param sample_vec vector of sample sizes
+#' @param alpha_level sign containing simulated log fold change used  for simulating the count data
+#' @param notu number of OTUs
+#' 
 #'
 power_fun_ss <- function(deseq_est_list,
                          true_logfoldchange,
